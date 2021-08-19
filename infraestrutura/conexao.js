305c09aa28d01633446e46fa0e826127ee9e4e1a
Tainara Almeida <68087649+taguinara@users.noreply.github.com>
2021-08-19 04:05:10 -0300

    Add files via upload

---
+++ infraestrutura/conexao.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2')
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '#Behappy3.',
+    database: 'agenda-petshop'
+})
+
+module.exports = conexao
